a211ddff13f3627089424a204d012626f3c01406
leebaojin <102916219+leebaojin@users.noreply.github.com>
2022-04-07 19:20:21 +0800

    Edited view and controllers

---
+++ ShoppingCartCA/Controllers/PurchaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShoppingCartCA.Models;
+
+namespace ShoppingCartCA.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["layoutheader"] = new LayoutHeader(null, new string[] { "Continue Shopping", "Logout" });
+            return View();
+        }
+    }
+}
