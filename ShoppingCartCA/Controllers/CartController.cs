39f85007ffd2d1777b1a91e9e8f0684264b1d7a7
leebaojin <102916219+leebaojin@users.noreply.github.com>
2022-04-07 18:02:34 +0800

    Edited CartController

--- ShoppingCartCA/Controllers/CartController.cs
+++ ShoppingCartCA/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ShoppingCartCA.Models;
 
 namespace ShoppingCartCA.Controllers
 {
@@ -10,6 +11,7 @@ namespace ShoppingCartCA.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["layoutheader"] = new LayoutHeader(null, new string[] { "Continue Shopping", "Checkout" });
             return View();
         }
     }
