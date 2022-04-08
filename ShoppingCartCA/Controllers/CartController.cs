d9ab055f568b4afb324a0ae020151307e851fb6a
leebaojin <102916219+leebaojin@users.noreply.github.com>
2022-04-09 01:48:57 +0800

    Implemented cart section

--- ShoppingCartCA/Controllers/CartController.cs
+++ ShoppingCartCA/Controllers/CartController.cs
@@ -28,23 +28,91 @@ namespace ShoppingCartCA.Controllers
         public IActionResult AddToCart(string productId, int qty=1)
         {
             //To autenticate the session
+            Customer customer = dbContext.Customers.FirstOrDefault(x => x.CustomerDetails.Username == "jeamsee");
 
-            //To create new cartdetail and add product in.
+            //Check if there is already this product
+            CartDetail cartDetail = customer.CartDetails.FirstOrDefault(x => x.Product.Id == Guid.Parse(productId));
+
+            if(cartDetail == null)
+            {
+                Product newProduct = dbContext.Products.FirstOrDefault(x => x.Id == Guid.Parse(productId));
+                if(newProduct == null)
+                {
+                    return Json(new { updateSuccess = false });
+                }
+                //To create new cartdetail and add product in.
+                customer.CartDetails.Add(new CartDetail()
+                {
+                    Product = newProduct,
+                    Quantity = 1
+                });
+            }
+            else
+            {
+                cartDetail.Quantity++;
+            }
+            dbContext.SaveChanges();
 
             //Return if successful and the new cart quantity
-            return Json(new { updateSuccess = true, cartqty=1 });
+            return Json(new { updateSuccess = true, cartqty= customer.CartDetails.Count });
         }
 
-        public IActionResult UpdateCartItem(string cartItemId, int qty)
+        public IActionResult UpdateCartItem([FromBody] CartUpdateData cartUpdateData )
         {
 
-            return Json(new { updateSuccess = true, cartqty = 1 });
-        }
+            Customer customer = dbContext.Customers.FirstOrDefault(x => x.CustomerDetails.Username == "jeamsee");
+            //CartDetail cartDetail = dbContext.CartDetails.FirstOrDefault(x => x.Id == Guid.Parse(cartUpdateData.CartItemId));
+            bool cartNotUpdated = true;
+            double totalcost = 0; double detailCost = 0;
+            Guid cartIdToUpdate = Guid.Parse(cartUpdateData.CartItemId);
 
-        public IActionResult RemoveCartItem(string cartItemId)
-        {
+            if(cartUpdateData.Newqty <= 0)
+            {
+                CartDetail cartDetailDelete = customer.CartDetails.FirstOrDefault(x => x.Id == cartIdToUpdate);
+                if(cartDetailDelete == null)
+                {
+                    return Json(new { updateSuccess = false });
+                }
+                customer.CartDetails.Remove(cartDetailDelete);
+               cartNotUpdated = false;
+            }
+            
+            foreach (CartDetail cartDetail in customer.CartDetails)
+            {
+                if(cartNotUpdated && cartDetail.Id == cartIdToUpdate)
+                {
+                        cartDetail.Quantity = cartUpdateData.Newqty;
+                    cartNotUpdated = false;
+                        detailCost = cartDetail.Quantity * cartDetail.Product.Price;
+                }
+                totalcost += cartDetail.Quantity * cartDetail.Product.Price;
+            }
+           
+            //cartDetail.Quantity = cartUpdateData.Newqty;
 
-            return Json(new { updateSuccess = true, cartqty = 1 });
+            if (cartNotUpdated)
+            { return Json(new { updateSuccess = false }); }
+            dbContext.SaveChanges();
+
+            if (cartUpdateData.Newqty > 0)
+            {
+                //If the row is not deleted
+                return Json(new
+                {
+                    updateSuccess = true,
+                    updateRow = cartUpdateData.UpdateRow,
+                    newqty = cartUpdateData.Newqty,
+                    price = String.Format("{0:0.00}", detailCost),
+                    totalprice = String.Format("{0:0.00}", totalcost)
+                });
+            }
+            return Json(new
+            {
+                updateSuccess = true,
+                updateRow = cartUpdateData.UpdateRow,
+                removeItem = true,
+                totalprice = String.Format("{0:0.00}", totalcost)
+            });
         }
     }
 }
---
+++ ShoppingCartCA/Models/CartUpdateData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCartCA.Models
+{
+    public class CartUpdateData
+    {
+        public string CartItemId { get; set; }
+
+        public int Newqty { get; set; }
+
+        public string UpdateRow { get; set; }
+    }
+}
