c5d7bab4b620d730b2f397585253825b0f8f63fa
leebaojin <102916219+leebaojin@users.noreply.github.com>
2022-04-08 11:48:18 +0800

    added template.cshtml in shared

---
+++ ShoppingCartCA/Views/Shared/ProductDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShoppingCartCA.Models;
+
+namespace ShoppingCartCA.Views.Shared
+{
+    public class ProductDisplay
+    {
+        public static string DisplayItem(Product product,string lastline = null)
+        {
+            string output = "<div class='prod-holder-table'>\n";
+
+            //add the product image
+            output += "<div class='prod-holder-row'>\n<div class='prod-col-img'>\n" +
+                "<img class='prod-img'src='" + product.Img+ "' />\n</div>\n</div>";
+
+            //add the product name
+            output += "<div class='prod-holder-row'>\n<div class='prod-col-title'>\n" +
+                product.Name + "</div>\n</div>";
+
+            //add the product 
+            output += "<div class='prod-holder-row'>\n<div class='prod-col-dec'>\n" +
+                product.Desc + "</div>\n</div>";
+
+            //add the button
+            output += "<div class='prod-holder-row'>\n<div class='prod-col-btn'>\n" +
+                "<input class='prod-button' type='button' action='/' value='$" + 
+                product.Price + " - Add to Cart' /></div>\n</div>";
+
+            return output;
+        }
+    }
+}
