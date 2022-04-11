5f40f791f8eeea9ddec1fd8e2b8b02acdb80e575
qchu002 <103117085+qchu002@users.noreply.github.com>
2022-04-11 18:07:25 +0800

    Coding of my checkout controller part.

--- ShoppingCartCA/Controllers/CheckoutController.cs
+++ ShoppingCartCA/Controllers/CheckoutController.cs
@@ -18,7 +18,66 @@ namespace ShoppingCartCA.Controllers
         public IActionResult Index()
         {
             ViewData["layoutheader"] = new LayoutHeader(null, new string[] { "Continue Shopping" });
+
+            Customer customer = SessionAutenticate.Autenticate(Request.Cookies["SessionId"], dbContext);
+            if (customer == null)
+            {
+                return RedirectToAction("Index", "Logout");
+            }
+            ViewData["allcartitem"] = customer.CartDetails;
+            ViewData["layoutheader"] = new LayoutHeader(customer, new string[] { "Continue Shopping" }, false);
+
             return View();
         }
-    }
+
+        public IActionResult AddToCart([FromBody] DataCartProduct datacartproduct)
+        {
+            Guid productId;
+            try
+            {
+                productId = Guid.Parse(datacartproduct.ProdId);
+            }
+            catch (Exception e)
+            {
+                return Json(new { addSuccess = false });
+            }
+            if (datacartproduct.Quantity <= 0)
+            {
+                return Json(new { addSuccess = false });
+            }
+
+            Customer customer = SessionAutenticate.Autenticate(Request.Cookies["SessionId"], dbContext);
+
+            if (customer == null)
+            {
+                return Json(new { addSuccess = false });
+            }
+
+            CartDetail cartDetail = customer.CartDetails.FirstOrDefault(x => x.Product.Id == productId);
+            if (cartDetail == null)
+            {
+                Product newProduct = dbContext.Products.FirstOrDefault(x => x.Id == productId);
+                if (newProduct == null)
+                {
+                    return Json(new { addSuccess = false });
+                }
+                //To create new cartdetail and add product in.
+                customer.CartDetails.Add(new CartDetail()
+                {
+                    Product = newProduct,
+                    Quantity = datacartproduct.Quantity
+                });
+            }
+            else
+            {
+                cartDetail.Quantity += datacartproduct.Quantity;
+            }
+            dbContext.SaveChanges();
+
+            return Json(new { addSuccess = true, cartqty = CartData.GetCartSize(customer) });
+
+        }
+
+    }    
+        
 }
