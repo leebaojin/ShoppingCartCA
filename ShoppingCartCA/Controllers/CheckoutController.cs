66ece58f5a8cdd0874dc75d27f7b96397ef5ef30
sherweipng <sherweipng@gmail.com>
2022-04-13 20:11:40 +0800

    Created methods to add cart details to customer and order, and clear cart after adding.

--- ShoppingCartCA/Controllers/CheckoutController.cs
+++ ShoppingCartCA/Controllers/CheckoutController.cs
@@ -15,6 +15,7 @@ namespace ShoppingCartCA.Controllers
         {
             this.dbContext = dbContext;
         }
+
         public IActionResult Index()
         {
             ViewData["layoutheader"] = new LayoutHeader(null, new string[] { "Continue Shopping" });
@@ -29,9 +30,75 @@ namespace ShoppingCartCA.Controllers
 
             return View();
         }
-        
-        
-        
+
+
+        public IActionResult SubmitOrder()
+        {
+            Customer customer = SessionAutenticate.Autenticate(Request.Cookies["SessionId"], dbContext);
+
+            Customer customerProfile = dbContext.Customers.FirstOrDefault(customerProfile => customerProfile.Id == customer.Id);
+
+            customerProfile.Orders.Add(GenerateOrder(customerProfile.CartDetails.ToList()));
+
+            dbContext.SaveChanges();
+
+            ClearCart(customerProfile.CartDetails.ToList());
+
+            return View("Redirect");/*RedirectToAction("Index", "Purchase")*/;
+        }
+
+        private Order GenerateOrder(List<CartDetail> allcartitem)
+        {
+            Order order = new Order
+            {
+                OrderDate = DateTime.Now,
+                OrderDetails = GenerateOrderDetails(allcartitem)
+            };
+
+            return order;
+        }
+
+        private List<OrderDetail> GenerateOrderDetails(List<CartDetail> allcartitem)
+        {
+            List<OrderDetail> orderDetails = new List<OrderDetail>();
+            foreach (CartDetail cartItem in allcartitem)
+            {
+                OrderDetail orderDetail = new OrderDetail
+                {
+                    Quantity = cartItem.Quantity,
+                    Product = cartItem.Product,
+                    PurchasePrice = cartItem.Product.Price,
+                    ActivationCodes = GenerateActivationCode(cartItem.Quantity, cartItem.Product)
+                };
+                orderDetails.Add(orderDetail);
+            }
+            return orderDetails;
+        }
+
+        private List<ActivationCode> GenerateActivationCode(int productQuantity, Product product)
+        {
+            List<ActivationCode> activationCodes = new List<ActivationCode>();
+
+            for (int i = 0; i < productQuantity; i++)
+            {
+                ActivationCode activationCode = new ActivationCode
+                {
+                    ProductId = product.Id
+                };
+                activationCodes.Add(activationCode);
+            }
+            return activationCodes;
+        }
+
+        private void ClearCart(List<CartDetail> allcartitem)
+        {
+            foreach (CartDetail cartItem in allcartitem)
+            {
+                dbContext.Remove(cartItem);
+            }
+            dbContext.SaveChanges();
+        }
+
     }
 
 }    
